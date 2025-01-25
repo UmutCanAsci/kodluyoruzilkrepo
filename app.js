4d3e6a68e0d15fcfb85c6a46edcca7efd4ab1a31
umutcanasc <umut.asci@bahcesehir.edu.tr>
2025-01-25 16:15:22 +0300

    Last version

---
+++ app.js
@@ -0,0 +1,7 @@
+// Select the button element
+const button = document.getElementById('myButton');
+
+// Add an event listener to the button
+button.addEventListener('click', () => {
+    alert('Button was clicked!');
+});
